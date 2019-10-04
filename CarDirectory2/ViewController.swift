c995ec86ea72ae3385f68f71549af65d7a56715f
Valentina Abramova <valentisha@gmail.com>
2019-10-04 15:19:00 +0300

    Initial Commit

---
+++ CarDirectory2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarDirectory2
+//
+//  Created by Valentina Abramova on 04/10/2019.
+//  Copyright © 2019 Valentina Abramova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
