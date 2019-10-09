d0cea84ec609e477573be7bee9b9f525ca22ca10
Valentina Abramova <valentisha@gmail.com>
2019-10-09 16:57:11 +0300

    Core Data Model

---
+++ CarDirectory2/Car+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Car+CoreDataClass.swift
+//  CarDirectory2
+//
+//  Created by Valentina Abramova on 09.10.2019.
+//  Copyright © 2019 Valentina Abramova. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Car)
+public class Car: NSManagedObject {
+
+}
---
+++ CarDirectory2/Car+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  Car+CoreDataProperties.swift
+//  CarDirectory2
+//
+//  Created by Valentina Abramova on 09.10.2019.
+//  Copyright © 2019 Valentina Abramova. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Car {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Car> {
+        return NSFetchRequest<Car>(entityName: "Car")
+    }
+
+    @NSManaged public var year: Int16
+    @NSManaged public var manufacturer: String?
+    @NSManaged public var model: String?
+    @NSManaged public var bodyType: String?
+    @NSManaged public var image: Data?
+
+}
